8a19e59cf60cc6a43d1e80b1380209f324c72017
Redhu <sunny.redhu@sap.com>
2020-05-16 21:47:46 +0530

    using react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import 'typeface-roboto';
+import Container from 'react-bootstrap/Container';
+import { Row, Col } from 'react-bootstrap';
+import GoogleButton from 'react-google-button';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      hello world
+    <div className='App'>
+      <Container>
+        <Row>
+          <Col xs={1} sm={3} />
+          <Col className='LoginMainGridItem' xs={10} sm={6} >
+            LOGIN
+            <hr />
+            <GoogleButton className='GoogleLoginButton'
+              onClick={() => { console.log('Google button clicked') }}
+            />
+          </Col>
+          <Col xs={1} sm={3} />
+        </Row>
+      </Container>
     </div>
   );
 }
