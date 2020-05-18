516aaf6dd31fffd4c349d267b6dcf344dff1dac7
Redhu <sunny.redhu@sap.com>
2020-05-18 20:27:24 +0530

    Implemented basic login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,114 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import { Row, Col } from 'react-bootstrap';
-import GoogleButton from 'react-google-button';
-import axios from 'axios';
+// imports begin here
+
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import axios from "axios";
 import { useHistory } from "react-router-dom";
+import MessageBox from './MessageBox';
+// imports end here
+
+// Login component begins here
 
 const Login = (props) => {
-  const history = useHistory();
+  // variables declaration begin here
 
-  const tryLoginByGoogle = async () => {
-    const jwtToken = await axios.post(
-      'http://localhost:3001/auth/local/login',
-      {
-        "email": "xykdsffgfdfjz@gmail.com",
-        "password": "Hello@123"
+  const history = useHistory();
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const [ displayMessageBox, setDisplayMessageBox ] = useState(false);
+  const [ messageBoxText, setMessageBoxText ] = useState("");
+
+  // variables declaration end here
+
+  // helper functions end here
+
+  const tryBasicLogin = async (event) => {
+    event.preventDefault();
+    // console.log(`Email: ${email}, Password: ${password}`);
+
+    try {
+      const jwtToken = await axios.post(
+        "http://localhost:3001/auth/local/login",
+        {
+          email: email,  // "xykdsffgfdfjz@gmail.com"
+          password: password,   // "Hello@123"
+        }
+      );
+  
+      if (jwtToken.status === 200) {
+        // console.log(`jwt token: ${jwtToken.data}`);
+        props.setJwt(jwtToken.data);
+        history.push(`/question`);
+      } else if (jwtToken.status === 401) {
+        throw new Error("Incorrect username or password!");
       }
-    );
-    if(jwtToken.status === 200) {
-      // console.log(`jwt token: ${jwtToken.data}`);
-      props.setJwt(jwtToken.data);
-      history.push(`/question`);
-    } else if(jwtToken.status === 401) {
-      console.log(`Couldn't login!`);
+    } catch(error) {
+      const msg = 'Incorrect username or password!';
+      console.log(msg);
+      setMessageBoxText(msg);
+      setDisplayMessageBox(true);
     }
-  }
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  // helper functions end here
+
+  // main rendering begins here
 
   return (
-    <div className='Login'>
+    <div className="Login">
+      <Container>
+        <Row>
+        <Col xs={1} sm={3} />
+        <Col xs={10} sm={6}>
+          <MessageBox
+            message={ messageBoxText }
+            variant="danger"
+            displayMessageBox={ displayMessageBox }
+            setDisplayMessageBox={ setDisplayMessageBox }
+          />
+        </Col>
+        <Col xs={1} sm={3} />
+        </Row>
+      </Container>
       <Container>
         <Row>
           <Col xs={1} sm={3} />
-          <Col className='LoginMainGridItem' xs={10} sm={6} >
+          <Col className="LoginMainGridItem" xs={10} sm={6}>
             LOGIN
             <hr />
-            <GoogleButton className='GoogleLoginButton'
-              onClick={ tryLoginByGoogle }
-            />
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" value={ email } onChange={ handleEmailChange } />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={ password } onChange={ handlePasswordChange } />
+              </Form.Group>
+              <Button variant="primary" type="submit" onClick={ tryBasicLogin }>
+                Submit
+              </Button>
+            </Form>
           </Col>
           <Col xs={1} sm={3} />
         </Row>
       </Container>
     </div>
   );
-}
+
+  // main rendering ends here
+};
+
+// Login component ends here
 
 export default Login;
