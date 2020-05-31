c2583ad94299e554f2417fa32586074dfa996580
Redhu <sunny.redhu@sap.com>
2020-05-31 20:07:44 +0530

    wip

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import SecureComponent from './SecureComponent';
 
 const Profile = (props) => {
-  console.log("sunny.profile: ", props);
+  const [ content, setContent ] = useState("This is the profile page");
+
+  useEffect(() => {
+    const getProfile = async () => {
+      try {
+        const profile = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/users/profile`);
+        if(profile) {
+          console.log(profile.data);
+          setContent(JSON.stringify(profile.data));
+        } else {
+          throw new Error("Couldn't receive profile from the server. Inform Sunny immediately!")
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    };
+
+    getProfile();
+  });
+
   return(
     <SecureComponent isLoggedIn={props.isLoggedIn} component={
       <div>
-        This is profile.
+        {content}
       </div>
     } />
   );
