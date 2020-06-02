a95e47c3883562a22ef5054b3f3ce75f21ecc160
Redhu <sunny.redhu@sap.com>
2020-06-02 17:36:55 +0530

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,25 +25,25 @@ const Home = (props) => {
       <h1>Frequently asked questions</h1>
       <ol>
         <li>
-          <h2>What is this app about?</h2>
+          <h3>What is this app about?</h3>
           <p>
             We believe that improving oneself is a continuous process.
             Hence, we have come up with a simple concept to improve one's problem solving skill.
-            That is just solve <b>ONE QUESTION PER DAY</b></p>.
+            That is just solve <b>ONE QUESTION PER DAY</b>.</p>
             By following this principle, you will not feel overwhelmed and you will learn something everyday.
         </li>
         <li>
-          <h2>Why this name JhaKhaas?</h2>
+          <h3>Why this name JhaKhaas?</h3>
           <p>
-            I got this idea from a person who gets a feeling of satisfaction by solving some kind aptitude problems.
-            First name which came to my mind was "Chalo Sawaal Banate Hain". But that's more like a tagline instead of name of an app.
+            I got this idea from a person who gets a feeling of satisfaction by solving aptitude problems.
+            First name which came to my mind was <b><i>Chalo Sawaal Banate Hain</i></b>. But that's more like a tagline instead of name of an app.
 
-            Then I thought, why not give it the name of that person who gave me the idea.
+            Then I thought, why not give it name of the person who gave me the idea.
             Hence, <b>Jha => Jha Parivar => Jha Specials => JhaKhaas</b> (which also means Awesome, Epic, Cool). 
           </p>
         </li>
         <li>
-          <h2>How to use this app?</h2>
+          <h3>How to use this app?</h3>
           <ul>
             <li>
               To login you need to have a gmail account.
@@ -72,8 +72,8 @@ const Home = (props) => {
           </ul>
         </li>
         <li>
-          <h2>How to contact the developer?</h2>          
-          Please reach out to me at <b><i>redhu.sunny1994@gmail.com</i></b> in case of any of the following cases:
+          <h3>How to contact the developer?</h3>          
+          Please reach out to me at <b><i>redhu.sunny1994@gmail.com</i></b> in any of the following cases:
           <ul>
             <li>
               You have good interesting problems you want on this app.
@@ -91,6 +91,10 @@ const Home = (props) => {
               Or just to say Hi!
             </li>
           </ul>
+        </li>
+        <li>
+          In the end, I would like to say:
+          <h4>Chalo Sawaal Banate Hain</h4>
         </li>
       </ol>
     </div>
